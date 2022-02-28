dcf400385264150ae3d002e8047f3c047be28e27
Gtp87 <fabio.giannitrapani@gmail.com>
2022-02-28 15:41:40 +0100

    create post table

---
+++ database/seeds/PostSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Model\Post;
+use Illuminate\Support\Str;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 50; $i++) {
+            $newPost = new Post();
+            $newPost->title = $faker->sentence(2, true);
+            $newPost->author = $faker->name();
+            $newPost->content = $faker->text();
+            $newPost->slug = Str::slug($newPost->title . '-' . $i, '-');
+            $newPost->save();
+        }
+    }
+}
